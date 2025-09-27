63cddbdb5455e31ba21bfae4ca5053324bf60653
sathish-ui6162 <sathish7494@gmail.com>
2025-09-28 00:17:49 +0530

    New Serice Changes

---
+++ LeanProOne/src/com/lean/service/ClientService.java
@@ -0,0 +1,15 @@
+package com.lean.service;
+
+public class ClientService {
+	
+	private String levelOne;
+
+	public String getLevelOne() {
+		return levelOne;
+	}
+
+	public void setLevelOne(String levelOne) {
+		this.levelOne = levelOne;
+	}
+
+}
--- LeanProOne/src/com/leanpro/LeanService.java
+++ LeanProOne/src/com/leanpro/LeanService.java
@@ -1,5 +1,38 @@
 package com.leanpro;
 
 public class LeanService {
+	
+	
+	private String name;
+	
+	private int age;
+	
+	private String dep;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getDep() {
+		return dep;
+	}
+
+	public void setDep(String dep) {
+		this.dep = dep;
+	}
+	
+	
 
 }
