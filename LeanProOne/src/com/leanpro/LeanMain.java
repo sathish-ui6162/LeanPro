8ebbd2d4e59257103c3e8ec49a19b44f67e2c0db
sathish-ui6162 <sathish7494@gmail.com>
2025-10-11 21:26:00 +0530

    Frame Added

--- LeanProOne/src/com/leanpro/LeanMain.java
+++ LeanProOne/src/com/leanpro/LeanMain.java
@@ -1,10 +1,15 @@
 package com.leanpro;
 
+import javax.swing.JFrame;
+
 public class LeanMain {
 
 	public static void main(String[] args) {
 		
 		
+		
+		
+		JFrame frame = new JFrame();
 		
 	System.out.println("Sathish Leena Jagath...");
 	
